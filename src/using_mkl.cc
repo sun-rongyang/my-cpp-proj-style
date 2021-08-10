82c60239fe48526c61307efa898948c1b8bbaf3e
Rong-Yang Sun <sun-rongyang@outlook.com>
2021-08-10 21:27:05 +0800

    try clang-format-check action fall state

--- src/using_mkl.cc
+++ src/using_mkl.cc
@@ -4,7 +4,8 @@
 
 int main(void) {
   MKLVersion Version;
-  mkl_get_version(&Version);
+  mkl_get_version(
+  &Version);
 
   printf("Major version:           %d\n", Version.MajorVersion);
   printf("Minor version:           %d\n", Version.MinorVersion);
